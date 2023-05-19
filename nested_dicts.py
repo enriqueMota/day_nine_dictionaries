81dd50a8884a3235363f92b8d44620b0d4908fde
enriqueMota <abelmota@live.com>
2023-05-19 10:30:55 -0400

    nested dictionaries

---
+++ nested_dicts.py
@@ -0,0 +1,29 @@
+# Nesting Dictionary in a Dictionary
+
+
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 34
+    }
+}
+
+
+# Nesting a Dictionary in a list
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 34
+    }
+]
